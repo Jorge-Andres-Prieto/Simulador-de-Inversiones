aabfa9664b1d68d56d2f2bb74c5fc86e6ba29a32
Jorge-prieto-jimenez <prietojorge905@gmail.com>
2024-04-22 06:10:30 -0500

    Se le aplican las pep8

--- simulador_inversiones.py
+++ simulador_inversiones.py
@@ -3,7 +3,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def calcular_inversion(inicial, tasa, años, adicional):
-    # Calcula el balance anual de la inversión con interés compuesto y aportes adicionales mensuales
+    """
+    Calcula el crecimiento de una inversión con interés compuesto y aportes
+    adicionales mensuales.
+
+    Args:
+        inicial (float): Monto inicial de la inversión.
+        tasa (float): Tasa de interés anual en porcentaje.
+        años (int): Duración de la inversión en años.
+        adicional (float): Aporte adicional mensual a la inversión.
+
+    Returns:
+        list: Lista de balances anuales de la inversión.
+    """
     balances = [inicial]
     for año in range(1, años + 1):
         final_año = balances[-1] * (1 + tasa / 100) + adicional * 12
@@ -11,6 +23,7 @@ def calcular_inversion(inicial, tasa, años, adicional):
     return balances
 
 def main():
+    """ Configura y ejecuta la aplicación Streamlit. """
     # Aplicar el tema personalizado
     st.set_page_config(page_title="Simulador de Inversiones 📈", page_icon="💹")
     st.markdown("""
@@ -25,14 +38,19 @@ def main():
     st.title('Simulador de Inversiones 🌱')
 
     # Entradas del usuario
-    cantidad_inicial = st.number_input('Cantidad inicial 💵 (en dólares)', min_value=0.0, value=1000.0, step=500.0)
-    tasa_interes = st.number_input('Tasa de interés anual (%) 📊', min_value=0.0, value=5.0, step=0.1)
-    años = st.number_input('Número de años 🗓️', min_value=1, max_value=50, value=10, step=1)
-    aporte_mensual = st.number_input('Aporte mensual adicional 💰 (en dólares)', min_value=0.0, value=100.0, step=50.0)
+    cantidad_inicial = st.number_input(
+        'Cantidad inicial 💵 (en dólares)', min_value=0.0, value=1000.0, step=500.0)
+    tasa_interes = st.number_input(
+        'Tasa de interés anual (%) 📊', min_value=0.0, value=5.0, step=0.1)
+    años = st.number_input(
+        'Número de años 🗓️', min_value=1, max_value=50, value=10, step=1)
+    aporte_mensual = st.number_input(
+        'Aporte mensual adicional 💰 (en dólares)', min_value=0.0, value=100.0, step=50.0)
 
     if st.button('Calcular 🧮'):
         # Procesamiento de los datos
-        balances = calcular_inversion(cantidad_inicial, tasa_interes, años, aporte_mensual)
+        balances = calcular_inversion(
+            cantidad_inicial, tasa_interes, años, aporte_mensual)
 
         # Mostrar resultados
         st.write(f"Balance final después de {años} años: ${balances[-1]:,.2f}")
